3765cefd995fb8602a52693ab3c82fa2f7090721
Romain Molina <molinaromain04@gmail.com>
2023-01-05 11:34:50 +0100

    Changed function GetUser, now works

--- src/controllers/User/handlers/getUser.ts
+++ src/controllers/User/handlers/getUser.ts
@@ -1,18 +1,14 @@
 import { RequestHandler } from "express";
 import { getModelUser } from "../../../models/User";
 
-export type UserRouteParams = {
-    id: number,
-    message: string
-}
 
-const getUser: RequestHandler<UserRouteParams> = (req, res, next) => {
-    try{
-        const user = getModelUser(req.params.id)
+const getUser: RequestHandler = async (req, res, next) => {
+    try {
+
+        const user = await getModelUser(parseInt(req.params.id));
         if (!user) throw new Error("user not found")
-
         res.json(user)
-    }catch(err){
+    } catch (err) {
         next(err);
     }
 }
--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,5 +1,5 @@
 
-import { PrismaClient,Post, Prisma } from '@prisma/client'
+import { PrismaClient, Post, Prisma } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
