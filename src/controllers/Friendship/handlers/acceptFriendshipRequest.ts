65d29bbaf332b57abc7d20286536db62b04941c3
Romain Molina <molinaromain04@gmail.com>
2023-01-12 11:58:48 +0100

    idk

--- src/controllers/Friendship/handlers/acceptFriendshipRequest.ts
+++ src/controllers/Friendship/handlers/acceptFriendshipRequest.ts
@@ -3,7 +3,7 @@ import { acceptFriendshipRequestModel } from '../../../models/Friendship';
 
 const acceptFriendshipRequest: RequestHandler = async (req, res, next) => {
     try {
-        const friendship = await acceptFriendshipRequestModel(req.body);
+        const friendship = await acceptFriendshipRequestModel(req.body.userId);
         res.json(friendship); //TODO zod
     } catch (err) {
         next(err);
--- src/routes/Friendship.ts
+++ src/routes/Friendship.ts
@@ -5,9 +5,9 @@ const FriendshipRouter = expres.Router();
 FriendshipRouter.post("/accept", Friendship.acceptFriendshipRequest);
 FriendshipRouter.post("/ignore", Friendship.ignoreFriendshipRequest);
 FriendshipRouter.post("/add", Friendship.addFriendshipRequest);
-FriendshipRouter.get("/requests", Friendship.getFriendshipRequests);
+FriendshipRouter.get("/requests/:userId", Friendship.getFriendshipRequests);
 FriendshipRouter.get("/:id", Friendship.getFriendship);
 FriendshipRouter.delete("/:id", Friendship.deleteFriendship);
 
-    
+
 export default FriendshipRouter;
