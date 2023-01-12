ddc2432bf9f876a7c715575d691f09e9a7987221
Francois BONNIN <francois.bonnin@ynov.com>
2023-01-12 10:15:49 +0100

    add cors policy for front-end

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,17 @@ import PostRouter from "./routes/Post";
 import PostLikeRouter from "./routes/PostLike";
 import PostCommentRouter from "./routes/PostComment";
 import FriendshipRouter from "./routes/Friendship";
+const cors = require("cors");
 
 const app = express();
+
+const corsOptions = {
+  origin : "http://localhost:3000",
+  credentials : true,
+  optionSuccessStatus : 200
+}
+
+app.use(cors(corsOptions));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -23,7 +32,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
 
 
 
