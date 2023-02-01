9dbf634a1b630780ed123cc493cb84cf2e3aefce
Molina Romain <molinaromain04@gmail.com>
2023-02-01 23:26:58 +0100

    sorry bébou

--- src/routes/Friendship.ts
+++ src/routes/Friendship.ts
@@ -6,6 +6,7 @@ FriendshipRouter.get("/:id", Friendship.getFriendship);
 FriendshipRouter.get("/friends/:userId", Friendship.getFriends);
 FriendshipRouter.post("/accept", Friendship.acceptFriendshipRequest);
 FriendshipRouter.post("/ignore", Friendship.ignoreFriendshipRequest);
+FriendshipRouter.get("/ignored/:userId", Friendship.getIgnoredFriendshipRequest);
 FriendshipRouter.post("/add", Friendship.addFriendshipRequest);
 FriendshipRouter.get("/requests/:userId", Friendship.getFriendshipRequests);
 FriendshipRouter.delete("/:id", Friendship.deleteFriendship);
